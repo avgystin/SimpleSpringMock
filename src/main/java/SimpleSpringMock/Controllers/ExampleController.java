726f68c9302811b2acafd9cfac73b095e365c9b5
HencH_MaN <oochucker65@gmail.com>
2022-07-23 20:20:02 +0300

    add sl4j logging

--- src/main/java/SimpleSpringMock/Controllers/ExampleController.java
+++ src/main/java/SimpleSpringMock/Controllers/ExampleController.java
@@ -1,13 +1,15 @@
 package SimpleSpringMock.Controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class ExampleController {
+    Logger logger = LoggerFactory.getLogger(ExampleController.class);
+
     @GetMapping(value = "/exampleGET")
     @ResponseStatus(HttpStatus.OK)
     public Object exampleGet() throws InterruptedException {
@@ -21,4 +23,5 @@ public class ExampleController {
         Thread.sleep(1000);
         return "POST SUCCESS"; // ответ в формате content-type: text/plain;charset=UTF-8
     }
+
 }

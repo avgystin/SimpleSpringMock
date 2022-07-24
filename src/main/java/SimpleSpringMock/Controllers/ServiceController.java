8a3262f68dfd560c049c65dd213b5203f7699281
HencH_MaN <oochucker65@gmail.com>
2022-07-24 13:18:03 +0300

    add comments #2

--- src/main/java/SimpleSpringMock/Controllers/ServiceController.java
+++ src/main/java/SimpleSpringMock/Controllers/ServiceController.java
@@ -1,10 +1,7 @@
 package SimpleSpringMock.Controllers;
 
-import SimpleSpringMock.Features.ConfigurationBeans;
 import SimpleSpringMock.Features.ResponseTimeConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;

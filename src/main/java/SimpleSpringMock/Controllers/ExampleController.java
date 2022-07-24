a2202413ccba6c055156da08fdc2f14cc4c8a341
HencH_MaN <oochucker65@gmail.com>
2022-07-24 14:36:38 +0300

    add simple correlation for XML response

--- src/main/java/SimpleSpringMock/Controllers/ExampleController.java
+++ src/main/java/SimpleSpringMock/Controllers/ExampleController.java
@@ -28,10 +28,11 @@ public class ExampleController {
         return "GET SUCCESS"; // ответ в формате content-type: text/plain;charset=UTF-8
     }
 
-    @PostMapping(value = "/examplePOST", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/examplePOSTJSON", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public Object examplePost(@RequestBody Map<String, String> request) throws InterruptedException {
-        request.replace("STATUS", "SUCCESS"); // заменяем {"STATUS":"ERROR"} на {"STATUS":"SUCCESS"}
+    public Object examplePostJSON(@RequestBody Map<String, String> request) throws InterruptedException {
+        // заменяем {"STATUS":"ERROR"} на {"STATUS":"SUCCESS"}
+        request.replace("STATUS", "SUCCESS");
         if (responseTimeBean.isDelaySwitch()) {
             Thread.sleep(responseTimeBean.getLongDelayMap().get("examplePOST"));
         } else {
@@ -40,4 +41,21 @@ public class ExampleController {
         return request; // ответ c корреляцией в формате content-type: application/json
     }
 
+    @PostMapping(value = "/examplePOSTXML", produces = MediaType.APPLICATION_XML_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public String examplePostXML(@RequestBody String request) throws InterruptedException {
+        // заменяем <REQUEST>
+        //    <STATUS>ERROR</STATUS>
+        //</REQUEST>
+        // на <RESPONSE>
+        //    <STATUS>SUCCESS</STATUS>
+        //</RESPONSE>
+        String response = request.replaceFirst("ERROR", "SUCCESS").replaceAll("REQUEST", "RESPONSE");
+        if (responseTimeBean.isDelaySwitch()) {
+            Thread.sleep(responseTimeBean.getLongDelayMap().get("examplePOST"));
+        } else {
+            Thread.sleep(responseTimeBean.getDelayMap().get("examplePOST"));
+        }
+        return response; // ответ c корреляцией в формате content-type: application/xml;charset=UTF-8
+    }
 }
